1b16c6d471f4b0bfd9f3bba43a91293f3f19da8b
Luan-Flores <keeganluan919@gmail.com>
2024-10-08 11:01:52 -0300

    almost done

--- cadastro.php
+++ cadastro.php
@@ -4,47 +4,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Funcionarios</title>
+    <link rel="stylesheet" href="style2.css">
+    
 </head>
 <body>
-<form action="" method="get">
-    <label for="nome">Nome: </label>
-    <input type="text" id="nome" name="nome" required>
-    
-    <label for="cargo">Cargo: </label>
-    <input type="text" id="cargo" name="cargo" required>
-    
-    <label for="idade">Idade: </label>
-    <input type="number" id="idade" name="idade" required>
-    
-    <label for="idade">Salario: </label>
-    <input type="float" id="idade" name="salario" required>
-    
-    <label for="telefone">Telefone: </label>
-    <input type="tel" id="telefone" name="telefone" required>
-    
-    <input type="submit" value="Enviar">
-</form>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    // Verifica se o formulário foi enviado
-    $nome = isset($_GET['nome']);
-    $cargo = isset($_GET['cargo']);
-    $idade = isset($_GET['idade']);
-    $salario = isset($_GET['salario']);
-    $telefone = isset($_GET['telefone']);
-
-    // Aqui você pode executar a lógica que precisar com os dados
-    echo "Nome: $nome <br>";
-    echo "Cargo: $cargo <br>";
-    echo "Idade: $idade <br>";
-    echo "Idade: $salario <br>";
-    echo "Telefone: $telefone <br>";
-}
-    $insert = $conexao->query(query:'insert into funcionario(nome, cargo, idade, salario, telefone) values
-    ($nome, $cargo, $idade, $salario , $telefone)');
-    var_dump($insert)
-    ?>
-    
+<div class="box-cadastro">    
+<div class='outdoor'>
+        <h1>Cadastro de Funcionários</h1>
+    </div>
+<section class="form-box">
+        <form action="" method="post">
+        <label for="nome">Nome: </label>
+        <input type="text" id="nome" name="nome" required>
+        
+        <label for="cargo">Cargo: </label>
+        <input type="text" id="cargo" name="cargo" required>
+        
+        <label for="idade">Idade: </label>
+        <input type="number" id="idade" name="idade" required>
+        
+        <label for="idade">Salario: </label>
+        <input type="float" id="idade" name="salario" required>
+        
+        <label for="telefone">Telefone: </label>
+        <input type="tel" id="telefone" name="telefone" required>
+        
+        <input type="submit" value="Enviar" id='enviar'>
+    </form>
+</section>
+</div>
 
 </body>
-</html>
+<?php
+$servidor ='localhost';
+$usuario ='root';
+$senha='';
+$banco_de_dados='caique1';
+
+$conexao = mysqli_connect(hostname: $servidor, username: $usuario, password: $senha, database: $banco_de_dados);
+if (isset($_POST['nome'],$_POST['cargo'],$_POST['idade'],$_POST['salario'],$_POST['telefone'])) {
+    
+    // Verifica se o formulário foi enviado
+    $nome = $_POST['nome'];
+    $cargo = $_POST['cargo'];
+    $idade = $_POST['idade'];
+    $salario = $_POST['salario'];
+    $telefone = $_POST['telefone'];
+    
+    
+    $insert = $conexao->query(query:"insert into funcionario(nome, cargo, idade, salario, telefone) values ('$nome', '$cargo', '$idade', '$salario' , '$telefone')");
+    
+    $conexao->close();
+}
+$conexao = mysqli_connect(hostname: $servidor, username: $usuario, password: $senha, database: $banco_de_dados);
+
+$selectFunc = $conexao->query(query: 'select * from funcionario');
+
+$rowsFunc = $selectFunc->fetch_all(MYSQLI_ASSOC);
+
+
+function renderTemplate($funcionario){
+    include "template.php";
+}
+
+foreach($rowsFunc as $funcionario){
+    renderTemplate($funcionario);
+}   
+$conexao->close();
+
+?>
+    
+    
+    </html>
--- template.php
+++ template.php
@@ -1,4 +1,8 @@
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style2.css">
+
+<div id="agaum">
+<h1>Funcionários Cadastrados</h1>
+</div>
 <div class="card-funcionario">
     <header>
         <h2>Nome: <?=$funcionario['nome']?></h2>
